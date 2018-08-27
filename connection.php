847700dacb8cc27623732b1050cbb02ac6114355
niyatisrinivasan <nsri0001@student.monash.edu>
2018-08-28 09:54:38 +1000

    Initial commit

---
+++ connection.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: niyatisrinivasan
+ * Date: 24/8/18
+ * Time: 12:29 PM
+ */
+
+$servername = "130.194.7.82";
+$username = "s29765706";
+$password = "monash00";
+$dbname = "S29765706";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    echo "Connected successfully";
