54c6383cd5bc8482a678867e29a48a5f119d3c57
niyatisrinivasan <nsri0001@student.monash.edu>
2018-08-29 13:01:12 +1000

    creation of properties page - database connection isn't working yet.

---
+++ properties.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: niyatisrinivasan
+ * Date: 29/8/18
+ * Time: 11:34 AM
+ */
+//$pStreet = $_POST["property_street"];
+//$pSuburb = $_POST["property_suburb"];
+//$pState = $_POST["property_state"];
+//$pPostalCode = $_POST["property_pc"];
+//$pDescription = $_POST["property_desc"];
+//$pListingDate = $_POST["listing_date"];
+
+if (empty($_POST["property_street"]))
+{
+     "Invalid error";
+}
+else
+{   include("connection.php");
+    $conn = new mysqli($servername, $username, $password, $dbname)
+    or die("Couldn't log on to database");
+    $query="INSERT INTO property (property_street, property_suburb, property_state, property_pc, property_desc, listing_date)
+            VALUES($_POST[property_street], $_POST[property_suburb],$_POST[property_state] ,$_POST[property_pc], $_POST[property_desc], $_POST[listing_date])";
+
+    $conn->query($query);
+}
