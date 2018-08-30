8490d9104518227ded66745aefb19566ac2d3b91
stra44 <stra44@student.monash.edu>
2018-08-30 22:09:45 +1000

    added credentials for connection to database

--- connection.php
+++ connection.php
@@ -5,11 +5,17 @@
  * Date: 24/8/18
  * Time: 12:29 PM
  */
-
+/*
 $servername = "130.194.7.82";
 $username = "s29765706";
 $password = "monash00";
 $dbname = "S29765706";
+*/
+$servername = "130.194.7.82";
+$username = "s27805247";
+$password = "monash00";
+$dbname = "S27805247";
+
 
 //$conn = mysqli_connect($servername, $username, $password, $dbname);
 //
