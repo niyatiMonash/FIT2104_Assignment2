ab5fae4f71efb1754583c25190df3d39b2012d40
stra44 <stra44@student.monash.edu>
2018-08-31 12:01:15 +1000

    Send Mail function to client

--- email-client.php
+++ email-client.php
@@ -8,12 +8,28 @@
 
 include("connection.php");
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-$query="SELECT * from client where client_mailinglist='Y'";
+$query="SELECT client_id, client_email from client where client_mailinglist='Y'";
 $result = mysqli_query($conn, $query);
 
 ?>
+<table>
+<?php
+while ($row = $result-->fetch_array()) {
+
+?>
+
+    <tr>
+        <td><?php echo $row["client_id"] ?></td>
+        <td><?php echo $row["client_email"] ?></td>
+        <td><a href="send-email.php?client_id= <?php echo $row["client_id"]; ?> &Action=SendEmail"></a>
+        </td>
+    </tr>
+
+
+</table>
 
 <?php
+}
 $recipient = "mail@example.com";
 $subject = "Subject of your email";
 $message = "Thank you for subscribing to our mailing list! Make sure you keep updated!";
---
+++ send-email.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stephanietran
+ * Date: 31/8/18
+ * Time: 11:58 AM
+ */
