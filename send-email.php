77c91b7c47ff39319c4fc9ada01a81819623d4f3
stra44 <stra44@student.monash.edu>
2018-09-07 12:19:45 +1000

    email

--- send-email.php
+++ send-email.php
@@ -1,3 +1,45 @@
+<html>
+<head>
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="stylesheets/modern-business.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="welcome.php">Ruthless Real Estate</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="edit-clients.php">Clients</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="add-properties.php">Properties</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="edit-type.php">Property Type</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="edit-feature.php">Property Feature</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="send-email.php">Send Email</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">Login</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
 <?php
 /**
  * Created by PhpStorm.
@@ -6,32 +48,67 @@
  * Time: 11:58 AM
  */
 include("connection.php");
-include("session.php");
-
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-$query="SELECT client_id, client_email from client where client_mailinglist='Y' and client_id=".$_GET["client_id"];
-$email_receipt = "niyati.srinivasan@gmail.com"; //$row["client_email"]; // to be changed TODO: Change default e-mail
+$query = "SELECT client_id, client_email, client_fname from client where client_mailinglist='Y'";
 $result = mysqli_query($conn, $query);
 
-$recipient = "mail@example.com";
-$subject = "Welcome to this month's issue of our newsletter";
-$message = "This month we have 10 new properties for purchase! Go down to our website to check them out!"; //TODO: change this text
-$headers =  'MIME-Version: 1.0' . "\r\n";
-$headers .= 'From: Ruthless Real Estate <nsri0001@student.monash.edu>'  . "\r\n"; //TODO: to create another email address and change this.
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+?>
 
-
-
-switch($_GET["Action"]) {
-    case "SendEmail":
-        mail($email_receipt, $subject, $message, $headers);
-        ?>
-        <script>
-                alert("Email sent!"); //TODO: alert box should popup only when mail sends successfully
-        </script>
 <?php
+if (!isset($_POST["to"])) {
 
+    ?>
+    <table>
+        <form method="post" action="send-email.php" align="center">
+
+            To: <select name="to">
+                <?php
+                while ($row = $result->fetch_array()) {
+                    ?>
+                    <option value="<?php echo $row["client_email"]; ?>"><?php echo $row["client_email"];
+                        ?>
+                    </option>
+                    <?php
+                }
+                ?>
+            </select> <br/>
+
+            Subject: <input type="text" name="subject"> <br/>
+            Message: <input type="text" name="message"> <br/>
+            <td>
+                <input type="submit" value="Send Email"/>
+                <input type="reset" value="clear"/>
+            </td>
+
+        </form>
+    </table>
+    <?php
+} else {
+    $from = "From: Ruthless Real Estate <nsri0001@student.monash.edu>";
+    $to = $_POST["to"];
+    $msg = $_POST["message"];
+    $subject = $_POST["subject"];
+    if (mail($to, $subject, $msg, $from)) {
+        echo "Email has been sent";
+    } else {
+        echo "Error Sending Mail";
+    }
 }
 ?>
+
+<footer class="py-5 bg-danger">
+    <div class="container-fluid">
+        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
+    </div>
+</footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
+
+
 
 
