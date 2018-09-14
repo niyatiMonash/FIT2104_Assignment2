5a23400aa6f3c2f5c11897523a5eca58dd5f5bb6
niyatisrinivasan <nsri0001@student.monash.edu>
2018-09-14 10:35:47 +1000

    HD feature: display property features for specific property. (WIP)

--- view-property.php
+++ view-property.php
@@ -9,12 +9,12 @@ include("session.php");
 //connection statement
 include("connection.php");
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-$query = "SELECT * FROM property p join client c on p.seller_id=c.client_id join type t on p.property_type=t.type_id 
-        WHERE property_id =" . $_GET["property_id"];
+$query = "SELECT * FROM property p 
+          join client c on p.seller_id=c.client_id 
+          join type t on p.property_type=t.type_id WHERE property_id =" . $_GET["property_id"];
 
 $result = $conn->query($query);
 $row = $result->fetch_assoc();
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -66,10 +66,68 @@ $row = $result->fetch_assoc();
 </nav>
 <div class="container-fluid">
     <!--    display property specific details-->
-    <?php echo $row["property_id"]; ?>
-    <?php echo $row["property_street"]; ?>
-    <?php echo $row["property_suburb"]; ?>
+    <img src="property_images/<?php echo $row["image_name"]; ?>" alt="property-image"
+         class="img-thumbnail rounded ">
+    <div class="property-details">
+        <h1> <?php echo $row["property_street"]; ?>, <?php echo $row["property_suburb"]; ?></h1>
+        <h2> <?php echo $row["property_state"]; ?><?php echo $row["property_pc"]; ?></h2>
+        <p>  <?php echo $row["property_desc"]; ?></p>
+    </div>
 
+    <div class="agent-details">
+        <p> Agent Details</p>
+    </div>
+    <div class="property-listing">
+        <p> <?php echo $row["listing_price"]; ?></p>
+        <p> <?php echo $row["listing_date"]; ?></p>
+    </div>
+
+    <div class="property-features">
+        <form method="post" action="view-property.php">
+            <table>
+                <thead>
+                <tr>
+                    <th>Feature Name</th>
+                    <th>Feature Description</th>
+                    <th>CheckBox</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                $query2 = "SELECT * FROM feature ORDER BY feature_name";
+
+                $result = $conn->query($query2);
+                while ($row = $result->fetch_assoc()) {
+                    $query3 = "SELECT * FROM property_feature 
+                               WHERE property_id =" . $_GET["property_id"] . " AND feature_id =" . $row["feature_id"];
+                    $result2 = $conn->query($query3);
+                    $pf = $result2->fetch_assoc();
+                    ?>
+                    <br>
+                    <td><?php echo $row["feature_name"] ?></td>
+                    <td><input type="text" name="feature_desc" value="<?php echo $pf["feature_desc"] ?>"</td>
+                    <?php
+                    if (isset($pf["feature_desc"])) {
+                        ?>
+                        <td><input type="checkbox" name="check" checked value="<?php echo $row["property_id"]; ?>"></td>
+                        <br/>
+                        <?php
+                    } else {
+                        ?>
+                        <td><input type="checkbox" name="check" value="<?php echo $row["property_id"]; ?>"></td><br/>
+                        <?php
+                    }
+                    ?>
+
+                    </tr>
+
+                    <?php
+                } ?>
+                </tbody>
+            </table>
+            <input type="submit" value="Update" class="btn btn-primary"/>
+        </form>
+    </div>
 </div>
 </body>
 
